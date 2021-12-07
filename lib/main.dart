6fccd4b08c63847194eb3a76657296b3a7b23dd8
Richard Dépierre <richard.habimana@epitech.eu>
2021-12-07 18:57:00 +0100

    feature:added home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+
+/*
+#F4EEEC
+#AEA19E
+#DFC2BE
+#CDB6B2
+#0A0A09
+#635D5C
+#F3293B
+#B572A9
+*/
 
 void main() {
   runApp(const MyApp());
@@ -6,22 +18,21 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Monest-eco',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Monest-eco'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
 
   @override
@@ -29,68 +40,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: const Padding(
+          padding: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+          child: CircleAvatar(
+            radius: 20,
+            backgroundImage: AssetImage("assets/images/perso.png"),
+          ),
+        ),
+        title: const Text(
+          "Chez Soso et Richou",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: const[
+          Padding(
+            padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+            child: Image(
+              image: AssetImage("assets/images/home.png"),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
+            child: Center(
+              child: Text(
+                "Energy Consumption today",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              "30€",
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              "20kWh",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.blue,
+        index: 2,
+        items: const <Widget>[
+          Icon(
+            Icons.lightbulb,
+            size: 30,
+            color: Colors.black,
+          ),
+          Icon(
+            Icons.people,
+            size: 30,
+            color: Colors.black,
+          ),
+          Icon(
+            Icons.home,
+            size: 30,
+            color: Colors.black,
+          ),
+          Icon(
+            Icons.person,
+            size: 30,
+            color: Colors.black,
+          ),
+          Icon(
+            Icons.settings,
+            size: 30,
+            color: Colors.black,
+          ),
+        ],
+      ),
     );
   }
 }
