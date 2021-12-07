a334afc5a931095be74e3d5d8fcf329906ee75f8
Richard Dépierre <richard.habimana@epitech.eu>
2021-12-07 19:11:01 +0100

    fixed: CI flutter analyze and flutter test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,2 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:monest/main.dart';
+
 void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    expect('0', '0');
+    expect('1', '1');
+    expect('0', '0');
+    expect('1', '1');
+  });
 }
