4d3b96fb767a1f644ee3d71b0e5be513bbbfad32
sophie <sophie.tromello-de-santis@epitech.eu>
2021-12-10 14:42:12 +0100

    add chart 1.0  of consum

---
+++ flutter_eip/lib/bar_gallery.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../gallery_scaffold.dart';
+import 'vertical_bar_label.dart';
+
+List<GalleryScaffold> buildGallery() {
+  return [
+    // ignore: unnecessary_new
+    new GalleryScaffold(
+      listTileIcon: Transform.rotate(
+          angle: 1.5708, child: const Icon(Icons.insert_chart)),
+      title: 'Vertical Bar Chart with Bar Labels',
+      subtitle: 'Vertical bar chart with a single series and bar labels',
+      childBuilder: () => VerticalBarLabelChart.withRandomData(),
+    )
+  ];
+}
---
+++ flutter_eip/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:charts_flutter/flutter.dart';
+
+import 'package:flutter/material.dart';
+import 'app_config.dart';
+import 'home.dart';
+
+/// The main gallery app widget.
+class GalleryApp extends StatefulWidget {
+  GalleryApp({Key? key}) : super(key: key);
+
+  @override
+  GalleryAppState createState() => new GalleryAppState();
+}
+
+/// The main gallery app state.
+///
+/// Controls performance overlay, and instantiates a [Home] widget.
+class GalleryAppState extends State<GalleryApp> {
+  // Initialize app settings from the default configuration.
+  bool _showPerformanceOverlay = defaultConfig.showPerformanceOverlay;
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: defaultConfig.appName,
+        theme: defaultConfig.theme,
+        showPerformanceOverlay: _showPerformanceOverlay,
+        home: new Home(
+          showPerformanceOverlay: _showPerformanceOverlay,
+          onShowPerformanceOverlayChanged: (bool value) {
+            setState(() {
+              _showPerformanceOverlay = value;
+            });
+          },
+        ));
+  }
+}
+
+void main() {
+  runApp(new GalleryApp());
+}
