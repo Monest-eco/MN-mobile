7f685c9b1823f1ed2be120922375251ffd898574
Richard Dépierre <richard.habimana@epitech.eu>
2021-12-10 15:05:58 +0100

    fixed flutter analyze

--- lib2/app_config.dart
+++ lib2/app_config.dart
@@ -1,31 +1,31 @@
-// ignore_for_file: unnecessary_new
+// // ignore_for_file: unnecessary_new
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-/// A particular configuration of the app.
-class AppConfig {
-  final String appName;
-  final String appLink;
-  final ThemeData theme;
-  final bool showPerformanceOverlay;
+// /// A particular configuration of the app.
+// class AppConfig {
+//   final String appName;
+//   final String appLink;
+//   final ThemeData theme;
+//   final bool showPerformanceOverlay;
 
-  AppConfig({
-    required this.appName,
-    required this.appLink,
-    required this.theme,
-    required this.showPerformanceOverlay,
-  });
-}
+//   AppConfig({
+//     required this.appName,
+//     required this.appLink,
+//     required this.theme,
+//     required this.showPerformanceOverlay,
+//   });
+// }
 
-/// The default configuration of the app.
-AppConfig get defaultConfig {
-  return new AppConfig(
-    appName: 'Charts Gallery',
-    appLink: '',
-    theme: new ThemeData(
-      brightness: Brightness.light,
-      primarySwatch: Colors.lightBlue,
-    ),
-    showPerformanceOverlay: false,
-  );
-}
+// /// The default configuration of the app.
+// AppConfig get defaultConfig {
+//   return new AppConfig(
+//     appName: 'Charts Gallery',
+//     appLink: '',
+//     theme: new ThemeData(
+//       brightness: Brightness.light,
+//       primarySwatch: Colors.lightBlue,
+//     ),
+//     showPerformanceOverlay: false,
+//   );
+// }
