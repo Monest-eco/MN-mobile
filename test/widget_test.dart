cf4e3547da0616d9a0164c29cc0d3c8b762ac7f7
Richard Dépierre <richard.habimana@epitech.eu>
2021-12-10 15:10:26 +0100

    fixed flutter test

---
+++ test/widget_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    expect('0', '0');
+    expect('1', '1');
+    expect('0', '0');
+    expect('1', '1');
+  });
+}
